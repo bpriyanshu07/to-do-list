535bab81bfdf94e646040b570db5f9732668493a
Priyanshu Bhandari <bpriyanshu525.com>
2026-09-19 21:39:47 +0530

    basic input setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,74 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const handleEdit = ()=>{
 
+  }
+
+  const handleChange = (e)=>{
+    setTodo(e.target.value)
+  }
+  
+  const handleDelete = ()=>{
+
+  }
+
+  const handleAdd = () =>{
+    setTodos([...todos,{todo, isCompleted: false}])
+    setTodo("")
+    console.log(todos)
+  }
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <Navbar />
+      <div className="bg-violet-200 container mx-auto my-1.5  rounded-xl p-2 min-h-[70vh] ">
+        <div className="addTodo">
+          <h2 className="text-xl font-bold">Add a Todo</h2>
+          <input
+            onChange={handleChange}
+            value={todo}
+            type="text"
+            className="bg-white w-80 px-1.5"
+          />
+          <button
+            onClick={handleAdd}
+            className=" text-white text-md font-bold bg-violet-800 hover:bg-violet-900  px-2 py-0.5 rounded-md mx-5"
+          >
+            Add
+          </button>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+        <h1 className="text-xl font-bold my-2 ">Your Todos</h1>
+        <div className="todos">
+          {todos.map((item) => {
+            return (
+              <div className="todo flex">
+                <div className={item.isCompleted ? "" : "line-through"}>
+                  {item.todo}
+                </div>
+                <div className="buttons">
+                  <button
+                    onClick={handleEdit}
+                    className=" text-white text-md font-bold bg-violet-800 hover:bg-violet-900  px-2 py-0.5 rounded-md mx-5"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={handleDelete}
+                    className=" text-white text-md font-bold bg-violet-800 hover:bg-violet-900  px-2 py-0.5 rounded-md mx-5"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
